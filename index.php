1afe7acef2697f900aba71c981c3bd2266a22cc3
Rustam <rustam@zestgeek.com>
2023-05-05 15:40:47 +0530

    changes in index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>docker file</title>
+</head>
+<body>
+<?php 
+
+echo "<h2>This is <i style='color:red'>Index File</i> running via Docker container</h2>";
+?>
+</body>
+</html>
