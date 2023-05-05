52fb7bf87e6fc4e840acde5d34e04daa2905aac6
Rustam <rustam@zestgeek.com>
2023-05-05 16:21:50 +0530

    new file added

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>docker file</title>
+	<title>Test file</title>
 </head>
 <body>
 <?php 
@@ -11,6 +11,5 @@
 echo "<h2>This is <i style='color:red'>Index File</i> running via Docker container</h2>";
 ?>
 <h2>Chnages for new git commit</h2>
-<h4>Chnages for new git commit</h4>
 </body>
 </html>
